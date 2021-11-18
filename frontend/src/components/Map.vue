3d085655315c3abd1d615979e20e902bf2eaba6b
Areon Lundkvist <areon.lundkvist@gmail.com>
2021-11-18 18:02:00 +0000

    Add map component

---
+++ frontend/src/components/Map.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="container">
+    <div id="mapContainer"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Map"
+}
+</script>
+
+<style scoped>
+  #mapContainer {
+    width: 25rem;
+    height: 50vh;
+  }
+</style>
