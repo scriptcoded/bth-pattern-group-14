85111bc2f66894eea787e5f396c8cb9f21f8c271
AreonL <areon.lundkvist@gmail.com>
2021-11-19 00:25:30 +0100

    feat: add more clickable liks/routes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     Navbar
+    <router-link :to="{name: 'home'}">Go to Home</router-link>
+    <router-link :to="{name: 'about'}">Go to About</router-link>
+    <router-link :to="{name: 'map'}">Go to Map</router-link>
 
     <router-view />
 
