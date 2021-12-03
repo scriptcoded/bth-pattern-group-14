c640feba7ef54c20abf801b1d70dd9beb41fe2f5
AreonL <areon.lundkvist@gmail.com>
2021-12-03 22:26:24 +0100

    fix: fixed routing to parking zone

--- api/src/router.js
+++ api/src/router.js
@@ -37,7 +37,7 @@ router.get('/parking-zones', parkingZoneController.getAllParkingZones)
 router.post('/parking-zones', parkingZoneController.createParkingZone)
 router.delete('/parking-zones/:id', parkingZoneController.deleteParkingZone)
 router.get('/parking-zones/:id', parkingZoneController.getOneParkingZone)
-router.patch('/charging-stations/:id', chargingStationController.updateParkingStation)
+router.patch('/parking-zones/:id', parkingZoneController.updateParkingStation)
 
 module.exports = {
   router
