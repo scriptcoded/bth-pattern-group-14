a8538c08a43ea43893ecd3d5e0a04826e72d2bb8
AreonL <areon.lundkvist@gmail.com>
2021-12-12 23:32:16 +0100

    feat: added history/transactions

--- frontend/src/components/profile/History.vue
+++ frontend/src/components/profile/History.vue
@@ -1,6 +1,42 @@
 <template>
   <div class="history">
-    <h1>Insert transaction | history</h1>
+    <div v-for="item in user" :key="item.id" class="stuffi"> <!-- Change id to type -->
+      <div v-if="item.disabled" class="ride-history"> <!--Change logic to match "history" -->
+        <div class="left">
+          <p>Datum: {{changeDate(item.createdAt)}}</p> <!-- Change argument -->
+          <p>Start: {{changeTime(item.createdAt)}}</p> <!-- Change argument -->
+          <p>End:   {{changeTime(item.updatedAt)}}</p> <!-- Change argument -->
+          <p>ID:    {{item.id}}</p>
+        </div>
+        <div class="right">
+          <p>Pris:   {{item.id}}</p> <!-- Change variable -->
+          <p>Rabatt: {{item.id}}</p> <!-- Change variable -->
+          <p>Summa:  {{item.id}}</p> <!-- Change variable -->
+        </div>
+      </div>
+      <div v-else-if="!item.disabled" class="faktura"> <!-- Change logic to match "Add to balance" -->
+        <div class="left">
+          <p>Inlagd balans: {{item.id}}</p> <!-- Change variable -->
+          <p>ID: {{item.id}}</p>
+        </div>
+        <div class="right">
+          <p>Datum: {{changeDate(item.createdAt)}}</p> <!-- Change argument -->
+          <p>Tid: {{changeTime(item.updatedAt)}}</p> <!-- Change argument -->
+        </div>
+      </div>
+      <div v-else class="faktura">
+        <div class="left">
+          <p>Faktura: {{item.id}}</p> <!-- Change variable -->
+          <p>ID: {{item.id}}</p>
+          <p>Betald: {{item.disabled ? "Ja" : "Nej"}}</p> <!-- Change variable -->
+        </div>
+        <div class="right">
+          <p>Datum utskick: {{changeDate(item.createdAt)}}</p> <!-- Change argument -->
+          <p v-if="item.disabled">Datum betald: {{changeDate(item.updatedAt)}}</p> <!-- Change argument && change if statement's variable-->
+          <p>Bästföre datum: {{changeDate(item.createdAt)}}</p> <!-- Change argument -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +45,7 @@
 export default {
   data () {
     return {
-      user: [],
+      user: {},
       history: []
     }
   },
@@ -17,6 +53,14 @@ export default {
     async userInfo () {
       const user = await this.$api.get('/bikes')
       this.user = user
+    },
+    changeTime (time) {
+      const actualDate = time.split('T')
+      return actualDate[1].substring(0, 8)
+    },
+    changeDate (time) {
+      const actualDate = time.split('T')
+      return actualDate[0]
     }
   },
   mounted () {
